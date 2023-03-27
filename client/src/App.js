76961204011c42e106cc4007ab34791a56158df5
bettitaksas <109687064+bettitaksas@users.noreply.github.com>
2023-03-27 18:00:57 +0200

    create header, filter type and gender

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import Animal from './components/Animal';
+import Header from './components/Header';
 
 function App() {
     const [allTheAnimals, setAllTheAnimals] = useState([]);
+    const [filteredAnimals, setFilteredAnimals] = useState([]);
 
     // env file-ba is rakhatjuk
     let key = 'Qnf0v9mZMNiNvet4d91zGjYvvE7NsOnMuBI7V7DZTRFowW4yFE';
@@ -12,48 +14,57 @@ function App() {
 
     // get authorization token
     useEffect(() => {
-      fetch('https://api.petfinder.com/v2/oauth2/token', {
-        method: 'POST',
-        body:
-            'grant_type=client_credentials&client_id=' +
-            key +
-            '&client_secret=' +
-            secret,
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-    })
-        .then((res) => res.json())
-        .then((data) => {
-            token = data.access_token;
-            console.log(data.access_token);
+        fetch('https://api.petfinder.com/v2/oauth2/token', {
+            method: 'POST',
+            body:
+                'grant_type=client_credentials&client_id=' +
+                key +
+                '&client_secret=' +
+                secret,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
         })
-        .then(() => {
-            // use token to fetch animals
-            fetch(
-                `https://api.petfinder.com/v2/animals?type=dog&location=90210`,
-                {
-                    method: 'GET',
-                    mode: 'cors',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        Authorization: 'Bearer ' + token,
-                    },
-                }
-            )
-                .then((res) => res.json())
-                .then((data) => console.log(data.animals));
-        })
-        .catch((err) => console.error(err));
-    }, [])
-    
+            .then((res) => res.json())
+            .then((data) => {
+                token = data.access_token;
+                console.log(data.access_token);
+            })
+            .then(() => {
+                // use token to fetch animals
+                fetch(
+                    `https://api.petfinder.com/v2/animals?type=dog&location=90210`,
+                    {
+                        method: 'GET',
+                        mode: 'cors',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            Authorization: 'Bearer ' + token,
+                        },
+                    }
+                )
+                    .then((res) => res.json())
+                    .then((data) => setAllTheAnimals(data.animals));
+                //.then((data) => console.log(data.animals))
+            })
+            .catch((err) => console.error(err));
+    }, []);
 
-// <Animal animal={animal} key={index}/>
+    // <Animal animal={animal} key={index}/>
     return (
         <div className='App'>
-{/*             {allTheAnimals.map((animal, index) => (
-               <Animal animal={animal} key={index}/>
-            ))} */}
+            <Header
+                key={0}
+                setFilteredAnimals={setFilteredAnimals}
+                allTheAnimals={allTheAnimals}
+            />
+            {filteredAnimals
+                ? filteredAnimals.map((animal, index) => (
+                      <Animal animal={animal} key={index} />
+                  ))
+                : allTheAnimals.map((animal, index) => (
+                      <Animal animal={animal} key={index} />
+                  ))}
         </div>
     );
 }
--- client/src/components/Animal.js
+++ client/src/components/Animal.js
@@ -4,6 +4,8 @@ export default function Animal({ animal }) {
     return (
         <>
             <div>{animal.name}</div>
+            <div>{animal.type}</div>
+            <div>{animal.gender}</div>
             <div>{animal.breeds.primary}</div>
         </>
     );
